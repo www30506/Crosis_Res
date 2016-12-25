f00e6d29c1a239f35b75b1e396b8eeef51151157
MSI\Crosis <www30506@gmail.com>
2016-12-25 22:54:20 +0800

    11

---
+++ DeepSpace GUI/scripts/MHScrollbarArrow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class MHScrollbarArrow : MonoBehaviour
+{
+    public Scrollbar target;
+    public float step = 0.1f;
+    public enum Direction
+    {
+        Up,
+        Down,
+    }
+
+    public Direction direction = Direction.Up;
+
+    public void OnPointerDown()
+    {        
+        if (target != null)
+        {
+            target.value += direction == Direction.Up ? step : -step;
+        }
+    }
+
+}
